82588d93474cb6cf6b57d887bb478d974efb8fbc
hsanjebri <hsan.jebri@esprit.tn>
2024-02-19 21:11:49 +0100

    spring-startup

---
+++ src/main/java/tn/care4elders/clinivia/CliniviaApplication.java
@@ -0,0 +1,13 @@
+package tn.care4elders.clinivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliniviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliniviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/care4elders/clinivia/CliniviaApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.care4elders.clinivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliniviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
