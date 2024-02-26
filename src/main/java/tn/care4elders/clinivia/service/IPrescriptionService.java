f3fbe0b539a83b2e7f2879c24731565a30e2d4cc
shadha <shadha@gmail.com>
2024-02-26 19:37:17 +0100

    shadha integrate crud

---
+++ src/main/java/tn/care4elders/clinivia/service/IPrescriptionService.java
@@ -0,0 +1,14 @@
+package tn.care4elders.clinivia.service;
+
+import tn.care4elders.clinivia.entity.Prescription;
+
+import java.util.List;
+
+public interface IPrescriptionService {
+    Prescription addPrescription(Prescription Prescription);
+    List<Prescription> getAllPrescriptions();
+    Prescription getPrescriptionById(long idPrescription);
+    void deletePrescription(long idPrescription);
+    Prescription updatePrescription(Prescription Prescription);
+
+}
