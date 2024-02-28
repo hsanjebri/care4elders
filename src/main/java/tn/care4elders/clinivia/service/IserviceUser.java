7cd5bb4b2303a33230e7c950b15a90af951a9887
dabbabimohamedamine <115744258+dabbabimohamedamine@users.noreply.github.com>
2024-02-28 12:42:01 +0100

    commit

--- src/main/java/tn/care4elders/clinivia/service/IserviceUser.java
+++ src/main/java/tn/care4elders/clinivia/service/IserviceUser.java
@@ -2,13 +2,14 @@ package tn.care4elders.clinivia.service;
 
 import tn.care4elders.clinivia.entity.User;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.Set;
+
 
 public interface IserviceUser {
     User addUser (User user);
     User updateUser(User user);
     void deleteUser(Long idUser);
     Optional<User> getUserById(Long idUser);
-    Set<User> getAllUsers();
+    List<User> getAllUsers();
 }
