63f661d48a24837930de7b4b70739b7222b5ef6e
Anis Khalfaoui <khalfaoui.anis@esprit.tn>
2024-03-04 13:31:06 +0100

    anis badel les entites

--- src/main/java/tn/care4elders/clinivia/config/test.java
+++
@@ -1,4 +0,0 @@
-package tn.care4elders.clinivia.config;
-
-public class test {
-}
--- src/main/java/tn/care4elders/clinivia/repository/IerviceRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.care4elders.clinivia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tn.care4elders.clinivia.entity.Iervice;
-
-@Repository
-public interface IerviceRepository extends JpaRepository<Iervice,Long> {
-}
