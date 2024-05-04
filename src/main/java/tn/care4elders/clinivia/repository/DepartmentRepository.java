19fb316de5597f73732954639d2680d8e4944948
maissabenghalba <115505822+maissabenghalba@users.noreply.github.com>
2024-05-04 14:13:53 +0200

    Revert "Push maissa"
    This reverts commit 6819aa57ae44c599f16fd046128a774d4101b7d9.

--- src/main/java/tn/care4elders/clinivia/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.care4elders.clinivia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tn.care4elders.clinivia.entity.Department;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department,Long> {
-}
--- src/main/java/tn/care4elders/clinivia/repository/PrescriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.care4elders.clinivia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.care4elders.clinivia.entity.Prescription;
-
-public interface PrescriptionRepository extends JpaRepository<Prescription,Long> {
-
-
-}
